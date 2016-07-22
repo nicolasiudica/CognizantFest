c5b037ca19723f7e5c9bee1e6a327afbd0f36e26
unknown <panella.dante@gmail.com>
2016-07-21 23:54:49 -0300

    First commit

---
+++ www/js/controllers.js
@@ -0,0 +1,30 @@
+angular.module('app.controllers', [])
+  
+.controller('homeCtrl', function($scope) {
+
+})
+   
+.controller('gamesCtrl', function($scope) {
+
+})
+   
+.controller('drinksCtrl', function($scope) {
+
+})
+      
+.controller('signupCtrl', function($scope) {
+
+})
+   
+.controller('playListCtrl', function($scope) {
+
+})
+   
+.controller('mapCtrl', function($scope) {
+
+})
+   
+.controller('cameraPhotosCtrl', function($scope) {
+
+})
+ 
