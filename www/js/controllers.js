937fdfb73c4a2e81a0ce4e3b78a19552cd89e058
Pastorino <529328@CTS.COM>
2016-08-25 10:31:00 -0300

    added drinks gallery

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -10,6 +10,27 @@ angular.module('app.controllers', [])
 	 
 .controller('drinksCtrl', function($scope, $state) {
 
+	  $scope.items = [{
+    src: 'img/cerveza.jpg'
+  }, {
+    src: 'img/daikiri.jpg',
+    sub: '<b>Daikiri ingredients are rum, strawberry and sugar.</b>'
+  }, {
+    src: 'img/fernet.jpg'
+  }, {
+    src: 'img/gintonic.jpg'
+  }, {
+  	src: 'img/cubalibre.jpg'
+  }, {
+  	src: 'img/destornillador.jpg'
+  }, {
+  	src: 'img/margarita.jpg'
+  }, {
+  	src: 'img/martini.jpg'
+  }, {
+    src: 'img/tequilaSunrise.jpg'
+  }];
+
 })
 			
 .controller('signupCtrl', function($scope, $state) {
