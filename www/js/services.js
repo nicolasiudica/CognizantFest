f110e4cb2799c0d936f542e37d8c30649a429d4b
Sanchez <396149@cognizant.com>
2016-08-25 17:06:21 -0300

    camera with db connection

--- www/js/services.js
+++ www/js/services.js
@@ -1,5 +1,10 @@
 angular.module('app.services', [])
 
+/*.factory("Gallery", function($firebaseArray) {
+    var firebase = new Firebase("https://cognizantfest.firebaseio.com/gallery");    
+    return $firebaseArray(itemsRef);
+})*/
+
 .factory('Camera', [function($q){
 
 }])
