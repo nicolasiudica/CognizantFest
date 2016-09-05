623ab4020ff85e4e147538d11694ff8dbd4aa37b
sdolfo <sdolfo@gmail.com>
2016-09-05 18:06:01 -0300

    random drink function and default empty modal functions

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,48 +1,141 @@
 angular.module('app.controllers', [])
 	
-.controller('homeCtrl', function($scope, $state) {
+.controller('homeCtrl', function($scope, $state, DaysLeftCounter) {
 	
+	$scope.daysLeft = DaysLeftCounter.day().daysLeft();
+
 })
 	 
 .controller('gamesCtrl', function($scope, $state) {
 
 })
 	 
-.controller('drinksCtrl', function($scope, $state) {
+.controller('drinksCtrl', function($scope, $state, $ionicModal, $cordovaDeviceMotion, $cordovaVibration) {
 
 	$scope.items = [
-  {
-    src: 'img/beer.png',
-    sub: '<b>Daikiri ingredients are rum, strawberry and sugar.</b>'
-  }, 
-  {
-    src: 'img/cubalibre.png',
-    sub: '<b>Daikiri ingredients are rum, strawberry and sugar.</b>'
-  }, 
-  {
-    src: 'img/fernet.png',
-    sub: '<b>Daikiri ingredients are rum, strawberry and sugar.</b>'
-  }, 
-  {
-    src: 'img/gintonic.png',
-    sub: '<b>Daikiri ingredients are rum, strawberry and sugar.</b>'
-  }, 
-  {
-    src: 'img/daikiri.png',
-    sub: '<b>Daikiri ingredients are rum, strawberry and sugar.</b>'
-  }, 
-  {
-    src: 'img/screwdriver.png',
-    sub: '<b>Daikiri ingredients are rum, strawberry and sugar.</b>'
-  }, 
-  {
-    src: 'img/margarita.png',
-    sub: '<b>Daikiri ingredients are rum, strawberry and sugar.</b>'
-  }, 
-  {
-    src: '',
-    sub: ''
-  }];
+		{
+			src: 'img/beer.png',
+			sub: '<b>Daikiri ingredients are rum, strawberry and sugar.</b>'
+	  	}, 
+		{
+			src: 'img/cubalibre.png',
+			sub: '<b>Daikiri ingredients are rum, strawberry and sugar.</b>'
+	  	}, 
+		{
+			src: 'img/fernet.png',
+			sub: '<b>Daikiri ingredients are rum, strawberry and sugar.</b>'
+	  	}, 
+		{
+			src: 'img/gintonic.png',
+			sub: '<b>Daikiri ingredients are rum, strawberry and sugar.</b>'
+	  	}, 
+		{
+			src: 'img/daikiri.png',
+			sub: '<b>Daikiri ingredients are rum, strawberry and sugar.</b>'
+	  	}, 
+		{
+			src: 'img/screwdriver.png',
+			sub: '<b>Daikiri ingredients are rum, strawberry and sugar.</b>'
+	  	}, 
+		{
+			src: 'img/margarita.png',
+			sub: '<b>Daikiri ingredients are rum, strawberry and sugar.</b>'
+	  	}, 
+		{
+			src: '',
+			sub: ''
+	  	}
+	];
+
+	$ionicModal.fromTemplateUrl('randomDrinkModal.html', {
+		scope: $scope,
+		animation: 'slide-in-up'
+	}).then(function(modal){
+		$scope.modal = modal;
+	});
+
+	$scope.onShakeTest = function(){
+  		console.log('hola soy el shake');
+  		var randomNumber = Math.floor((Math.random() * 7));
+		console.log('your random drink is: ' + randomNumber);
+		console.log($scope.items[randomNumber].src)
+
+		//OPEN MODAL WITH RANDOM DRINK
+		$scope.modal.show();
+	}
+
+	$scope.closeModal = function(){
+		$scope.modal.hide();
+		console.log('escondiendo la modal')
+	}
+
+  	var onShake = function () {
+  		$('#pruebaShake').html("<strong>SHAKE DETECTADO</strong>");
+  		$cordovaVibration.vibrate(500);
+  		var url = randomDrink(cantidadTragos);
+  		$('#pruebaShake').html("<strong>URL: " + url + "</strong>");
+	};
+
+	var onError = function () {
+	  $('#pruebaShake').html("<strong>ERROR DETECTADO</strong>");
+	  // Fired when there is an accelerometer error (optional)
+	};
+
+	// Start watching for shake gestures and call "onShake"
+	// with a shake sensitivity of 40 (optional, default 30)
+	if(window.shake) shake.startWatch(onShake, 30 , onError);
+
+
+	 //defino la cantidad de tragos y los nombres
+	var cantidadTragos = 9;
+	//este es el resultado rel return, puede ser la imagen o una url, o un div, depende de como se haga la interfaz
+	trago1 = "img/cerveza.jpg";
+	trago2 = "img/martini.jpg";
+	trago3 = "img/daikiri.jpg";
+	trago4 = "img/margarita.jpg";
+	trago5 = "img/cubalibre.jpg";
+	trago6 = "img/tequilaSunrise.jpg";
+	trago7 = "img/fernet.jpg";
+	trago8 = "img/gintonic.jpg";
+	trago9 = "img/destornillador.jpg";
+
+	//ejecuto la funcion para elegir el trago luego de que se detecte el shake event
+	function randomDrink(cantidadTragos){
+	  var idTrago = Math.floor((Math.random() * cantidadTragos) + 1);
+	  switch(idTrago) {
+		case 1:
+			tragoAleatorio = trago1;
+			break;
+		case 2:
+			tragoAleatorio = trago2;
+			break;
+		case 3:
+			tragoAleatorio = trago3;
+			break;
+		case 4:
+			tragoAleatorio = trago4;
+			break;
+		case 5:
+			tragoAleatorio = trago5;
+			break;
+		case 6:
+			tragoAleatorio = trago6;
+			break;
+		case 7:
+			tragoAleatorio = trago7;
+			break;
+		case 8:
+			tragoAleatorio = trago8;
+			break;
+		case 9:
+			tragoAleatorio = trago9;
+			break;
+		case 10:
+			tragoAleatorio = trago10;
+			break;
+		}
+	  	return tragoAleatorio;
+	}
 
 })
 			
@@ -132,8 +225,8 @@ angular.module('app.controllers', [])
 	destination_input.value = partyLocation;    
 	initMap();
 	
- 	//Initialize map
- 	function initMap(){
+	//Initialize map
+	function initMap(){
 		var options = {timeout:1000, enableHighAccuracy:true};
 	  
 		$cordovaGeolocation.getCurrentPosition(options).then(function(position){
@@ -354,7 +447,7 @@ angular.module('app.controllers', [])
 			} else {
 				console.log('***** calculateAndDisplayRoute Directions request failed due to ' + status);
 		}
-	  	});
+		});
 	}
 
 	//Draw route
--- www/js/services.js
+++ www/js/services.js
@@ -1,5 +1,31 @@
 angular.module('app.services', ['ngCordova'])
 
+// Days countdown to the party
+.service('DaysLeftCounter', [function(){
+
+	this.day = function(){
+
+		var today = new Date();
+		var PartyDay = new Date("November 10, 2016");
+
+		// Milisegundos en un dia
+		const msDay = 24 * 60 * 60 * 1000;
+		// Tiempo en milisegundos que falatan para la fiesta
+		var timeLeft = (PartyDay.getTime() - today.getTime());
+		var daysLeft = Math.floor(timeLeft / msDay);
+
+		return {
+			daysLeft: function() {
+			  return daysLeft;
+			},
+			partyDay: function() {
+				return PartyDay;
+			}
+		}
+	}
+
+}])
+
 .service('BlankService', [function(){
 
 }]);
