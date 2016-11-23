df7bc7ff8571efe35c2451dd3e188de40eaf242e
Lautaro Tejerina <lautaro.tejerina@cognizant.com>
2016-11-23 12:28:22 -0300

    New slide login and wait for image loading

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -6,15 +6,60 @@ angular
 
 .controller('photosCtrl', ['FirebaseDB', '$scope', '$timeout',
 						   function (FirebaseDB, $scope, $timeout) {
-
 		var databaseRef = FirebaseDB.database().ref("CogniFest/photos");
+		$scope.loadedImages = [];
+
+		// Watching for imageloaded event to later start autoplaying when all loaded
+		$scope.$on('imageloaded', function (evt, data) {
+			//console.log("imageloaded", evt, data)
+			$scope.loadedImages.push(data);
+			console.log("----> Loaded", $scope.loadedImages.length, "images");
+			$scope.$apply();
+		});
+
+		$scope.$watchCollection('loadedImages', function (newVal, oldVal, scope) {
+			var status = areAllLoaded(newVal.length);
+			console.log("loaded status", status);
+			if (status) {
+				console.log("Since all photos are loaded, start autoplay");
+				forceStartAutoplay($scope.data.sliderDelegate);
+			}
+		}, true);
+
+		function updateSlideCounter(swiper) {
+			console.log("Updating counter");
+			if (!swiper) {
+				console.log("No swiper instance received", swiper);
+				return;
+			}
+			var currentIndex = swiper.activeIndex;
+			var totalIndex = $scope.data.firebasePhotos.length;
+			console.log("Current index:", currentIndex, "Total index:", totalIndex);
+			$scope.data.currentIndex = currentIndex + 1;
+			$scope.data.totalIndex = totalIndex;
+			$scope.$apply();
+		}
+
+		function areAllLoaded(loadedLength) {
+			var totalLength = $scope.data.firebasePhotos.length;
+			var status = (loadedLength === totalLength);
+			console.log("loaded", loadedLength, "total", totalLength);
+			return status;
+		}
+
+		function forceStartAutoplay(sliderDelegate) {
+			$timeout(sliderDelegate.stopAutoplay())
+				.then(sliderDelegate.startAutoplay());
+		}
 
 		$scope.data = {};
 		$scope.data.firebasePhotos = [];
 
+		// Watching for new pictures
 		databaseRef.on("value", function (snapshot) {
 			var firebasePhotos = [];
-
+			// Load all pictures into an array...
+			console.log("Load all pictures into an array...");
 			snapshot.forEach(function (childsnap) {
 				firebasePhotos.push({
 					owner: childsnap.child('owner').val(),
@@ -22,17 +67,23 @@ angular
 					time: childsnap.child('time').val()
 				});
 			});
-
+			// ...so now I can see how many are new and separate them...
+			console.log("...so now I can see how many are new and separate them...");
 			var newPhotos = _.differenceBy(firebasePhotos, $scope.data.firebasePhotos, 'src');
-
+			// ...to later add them to the $scope...
+			console.log("...to later add them to the $scope...");
 			newPhotos.forEach(function (newPhoto) {
 				$scope.data.firebasePhotos.push(newPhoto);
 			});
-
+			// ...and update the slide counter...
+			console.log("...and update the slide counter...");
+			$scope.data.sliderDelegate.on('slideChangeStart', updateSlideCounter);
+			$scope.data.sliderDelegate.on('slideChangeEnd', updateSlideCounter);
+			$scope.data.sliderDelegate.on('init', updateSlideCounter);
+			// ...and the slider instance.
+			console.log("...and the slider instance.");
 			$timeout($scope.$apply())
-				.then($scope.data.sliderDelegate.update())
-				.then($scope.data.sliderDelegate.slideTo(0))
-				.then($scope.data.sliderDelegate.slideNext());
+				.then($scope.data.sliderDelegate.update());
 
 			console.log("---> Gallery has", $scope.data.firebasePhotos.length, "images", $scope.data.firebasePhotos);
 		});
@@ -42,10 +93,11 @@ angular
 			grabCursor: true,
 			centeredSlides: true,
 			initialSlide: 0,
-			loop: true,
-			speed: 500,
+			loop: false,
+			speed: 2000,
 			autoplay: 1000,
 			autoplayDisableOnInteraction: false,
+			autoplayStopOnLast: true,
 			coverflow: {
 				rotate: 50,
 				stretch: 0,
--- www/js/directives.js
+++ www/js/directives.js
@@ -1,6 +1,57 @@
 angular.module('app.directives', [])
 
-.directive('blankDirective', [function(){
+.directive('imgPreload', ['$rootScope', function ($rootScope) {
+	return {
+		restrict: 'A',
+		transclude: true,
+		scope: {
+			ngSrc: '@'
+		},
+		link: function (scope, element, attrs) {
+			// triggered after each item is loaded
+			function onProgress(imgLoad, image) {
+				//console.log("onProgress", imgLoad, image);
+				// change class if the image is loaded or broken
+				var $item = $(image.img).parent();
+				$item.removeClass('is-loading');
+				if (!image.isLoaded) {
+					$item.addClass('is-broken');
+				}
+				var result = image.isLoaded ? 'loaded' : 'broken';
+				//console.log('image is ' + result + ' for ' + image.img.src);
+			}
 
+			function onAlways(imgLoad) {
+				console.log(imgLoad.images.length + ' images loaded');
+				// detect which image is broken
+				//for (var i = 0, len = imgLoad.images.length; i < len; i++) {
+				//	var image = imgLoad.images[i];
+				//	var result = image.isLoaded ? 'loaded' : 'broken';
+				//	console.log('image is ' + result + ' for ' + image.img.src);
+				//}
+			}
+
+			function onDone(imgLoad) {
+				//console.log("image correctly loaded");
+				$rootScope.$broadcast('imageloaded', imgLoad.images[0].img.src);
+			}
+
+			element.imagesLoaded()
+			//	.always(onAlways)
+				.done(onDone)
+				.fail(function () {
+					console.log('all images loaded, at least one is broken');
+				})
+				.progress(onProgress);
+			//.progress(onProgress)
+			//.always(onAlways);
+			//element.bind('load', function () {
+			//	//call the function that was passed
+			//	scope.$apply(attrs.imageonload);
+			//});
+			//scope.$watch('ngSrc', function (newVal) {
+			//	scope.$apply(attrs.imgPreload);
+			//});
+		}
+	};
 }]);
-
